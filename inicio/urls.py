1bee18ec15421bb245c9f7a3a665505983409650
Luciano Timo <lucianotimo@betwarrior.com>
2024-05-30 20:56:45 -0300

    se agrega base de datos y bootstrap

---
+++ inicio/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from inicio import views
+
+
+urlpatterns = [
+    path('', views.inicio),
+    path('template1/', views.template1),
+    path('template4/<str:nombre>/<str:apellido>/<int:edad>', views.template4),
+    path('probando/', views.probando),
+]
+
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from inicio.views import inicio, template1, template4, probando
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
-    path('template1/', template1),
-    path('template4/<str:nombre>/<str:apellido>/<int:edad>', template4),
-    path('probando_if_for/', probando),
+    path('', include('inicio.urls')),
 ]
-
